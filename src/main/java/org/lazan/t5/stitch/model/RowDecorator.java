c77a587b789145c29fc546c018a41af8b71a7c2a
uklance <uklance@gmail.com>
2012-11-11 21:41:34 +0000

    Deleted old components

--- src/main/java/org/lazan/t5/stitch/model/RowDecorator.java
+++
@@ -1,7 +0,0 @@
-package org.lazan.t5.stitch.model;
-
-import org.apache.tapestry5.dom.Element;
-
-public interface RowDecorator {
-	public void decorate(Element element, Object rowValue, int rowIndex);
-}

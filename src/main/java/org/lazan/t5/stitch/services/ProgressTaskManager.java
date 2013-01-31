6a4177a084811491b47ac6b0ff1a0c63f49c65c8
Lance <uklance@gmail.com>
2013-01-31 15:21:28 +0000

    Update src/main/java/org/lazan/t5/stitch/services/ProgressTaskManager.java

--- src/main/java/org/lazan/t5/stitch/services/ProgressTaskManager.java
+++ src/main/java/org/lazan/t5/stitch/services/ProgressTaskManager.java
@@ -3,6 +3,19 @@ package org.lazan.t5.stitch.services;
 import org.lazan.t5.stitch.model.ProgressTask;
 
 public interface ProgressTaskManager {
+	/**
+	 * Get the current progress of the task.
+	 * Progress will be a value between 0 (no progress) and 1 (finished)
+	 * 
+	 * @param taskId The id of the task submitted by a previous call to add(ProgressTask)
+	 * @return The progress value
+	 */
 	float getProgress(int taskId);
+	
+	/**
+	 * Add a task to be executed in parallel. This can be queried for it's progress
+	 * 
+	 * @return The taskId assigned to the task
+	 */
 	int add(ProgressTask progressTask);
 }

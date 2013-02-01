5214e1342b29608f3f5a9abd0a9720a633aa4bdd
Lance <uklance@gmail.com>
2013-02-01 10:14:05 +0000

    Update src/main/java/org/lazan/t5/stitch/model/ProgressTask.java

--- src/main/java/org/lazan/t5/stitch/model/ProgressTask.java
+++ src/main/java/org/lazan/t5/stitch/model/ProgressTask.java
@@ -1,5 +1,11 @@
 package org.lazan.t5.stitch.model;
 
+/**
+ * A RrogressTask is a Runnable that can be queried periodically for
+ * it's progress. Implementors must be aware that getProgress() will
+ * polled asynchronously whilst the task is executing and may require
+ * synchronization.
+ */
 public interface ProgressTask extends Runnable {
 	/**
 	 * Get the percentage complete of the task
